34e1baf6de6fc9023110f12d9f8ed92f6ce61152
luetkemj <luetkemj@gmail.com>
2021-09-18 17:44:47 -0500

    log entities at location on click

--- src/lib/canvas.js
+++ src/lib/canvas.js
@@ -1,6 +1,9 @@
 import _ from "lodash";
 import * as PIXI from "pixi.js";
-// add PIXI to the window so the devtool work
+import { Position } from "../components";
+import { getEntityData } from "./ecsHelpers";
+
+// add PIXI to the window so the devtools work
 window.PIXI = PIXI;
 
 const canvas = document.getElementById("pixi-canvas");
@@ -23,8 +26,18 @@ export const addSprite = ({ texture, options = {}, world, eid }) => {
         x: 0.5,
         y: 0.5,
       },
+      interactive: true,
     },
     options
   );
+
+  world.sprites[eid].on("click", (ev) => {
+    const x = Position.x[eid];
+    const y = Position.y[eid];
+    const z = Position.z[eid];
+    const pos = `${x},${y},${z}`;
+    world.eAtPos[pos].forEach((e) => console.log(getEntityData(world, e)));
+  });
+
   app.stage.addChild(world.sprites[eid]);
 };
