d46526cbf2d30773f923d438f9d068006bd032a6
luetkemj <luetkemj@gmail.com>
2021-09-22 09:46:57 -0500

    WIP

--- src/lib/canvas.js
+++ src/lib/canvas.js
@@ -20,6 +20,58 @@ const app = new PIXI.Application({
   resolution: window.devicePixelRatio || 1,
 });
 
+// TEST
+const loader = PIXI.Loader.shared; // PixiJS exposes a premade instance for you to use.
+
+loader.add("static/fonts/courier-prime-regular.json").load(setup);
+
+function setup() {
+  const spriteSheets = {};
+
+  spriteSheets.courierPrimeRegular =
+    loader.resources["static/fonts/courier-prime-regular.json"];
+
+  // create sprites for each cell on grid.log
+  _.times(grid.log.width * 2, (i) => {
+    const letter = new PIXI.Sprite(
+      spriteSheets.courierPrimeRegular.textures["courier-prime-regular-0.png"]
+    );
+    letter.width = cellHfW;
+    letter.height = cellW;
+    letter.x = i * cellHfW;
+    containers.log.addChild(letter);
+  });
+
+  // function that takes a string and updates the textures for each sprite at the correct location
+}
+
+// function setup() {
+//   // get a reference to the sprite sheet we've just loaded:
+//   let sheet = loader.resources["static/test/spritesheet.json"];
+
+//   // initialize background sprite
+//   const knife = new PIXI.Sprite(sheet.textures["goblin_knife.png"]);
+//   knife.x = cellW;
+
+//   // add it to the stage
+//   containers.log.addChild(knife);
+
+//   console.log(sheet);
+
+//   // create an animated sprite
+//   const animatedGoblin = new PIXI.AnimatedSprite(
+//     sheet.spritesheet.animations["goblin_idle_anim_f"]
+//   );
+
+//   // set speed, start playback and add it to the stage
+//   animatedGoblin.animationSpeed = 0.167;
+//   animatedGoblin.play();
+//   animatedGoblin.width = cellW * 4;
+//   animatedGoblin.height = cellW * 4;
+//   containers.log.addChild(animatedGoblin);
+// }
+// END TEST
+
 const containers = {};
 
 containers.map = new PIXI.Container();
@@ -36,6 +88,7 @@ containers.log.y = grid.log.y * cellW + cellHfW;
 
 app.stage.addChild(containers.map);
 app.stage.addChild(containers.log);
+
 export const addDebugSprite = ({ texture, x, y }) => {
   const sprite = new PIXI.Sprite.from(texture);
   sprite.x = x;
