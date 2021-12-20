766166bf5b07dbe15905b0e2739959898fb086fa
luetkemj <luetkemj@gmail.com>
2021-12-19 18:46:31 -0600

    header, footer and colored log groups

--- src/ui/menuLog.js
+++ src/ui/menuLog.js
@@ -7,7 +7,8 @@ const container = "menu";
 
 export const renderMenuLog = () => {
   const { log, rowIndex } = getState().log;
-  const logHeight = grid.menu.height;
+  // THE ERROR IS HERE - we are getting more logs than we can show because we're adding height with the spacers....
+  const logHeight = grid.menu.height - 5;
 
   let logRowIndex = rowIndex;
 
@@ -29,13 +30,37 @@ export const renderMenuLog = () => {
     setState((state) => (state.log.rowIndex = 0));
   }
 
-  const visibleLog = log.slice(logRowIndex, logRowIndex + grid.menu.height);
+  const visibleLog = log.slice(logRowIndex, logRowIndex + logHeight);
 
+  printTemplate({
+    container,
+    template: [{ str: " -- ADVENTURER'S LOG --" }],
+    y: 1,
+  });
+
+  let y = 3;
+
+  let rollingTick = -1;
+  let rollingTurn = 1;
   for (const [i, l] of visibleLog.entries()) {
+    if (l.tick !== rollingTick) {
+      rollingTick = l.tick;
+      rollingTurn++;
+    }
+
+    const color = rollingTurn % 2 ? 0x00ff77 : 0x58cced;
     printTemplate({
       container,
-      template: l.log,
-      y: i,
+      template: [{ str: `${l.tick}-`, color }, ...l.log],
+      y: y,
+      x: 4,
     });
+    y++;
   }
+
+  printTemplate({
+    container,
+    template: [{ str: `(esc)Return to game  (arrow keys)Scroll ` }],
+    y: logHeight + 4,
+  });
 };
